f5d80fc09ce6b1af458ec424d1ea60196e69fe16
Karina Denisevich <karina_41997@mail.ru>
2016-07-28 13:32:14 +0300

    master: configuration methods added

--- src/main/java/com/github/Karina_Denisevich/spring/data/jpa/example/config/DataConfig.java
+++ src/main/java/com/github/Karina_Denisevich/spring/data/jpa/example/config/DataConfig.java
@@ -1,5 +1,6 @@
 package com.github.Karina_Denisevich.spring.data.jpa.example.config;
 
+import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,13 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -24,6 +28,7 @@ public class DataConfig {
 
     @Bean
     public DataSource dataSource() {
+
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
         dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
@@ -32,6 +37,41 @@ public class DataConfig {
         dataSource.setPassword(environment.getRequiredProperty("db.password"));
 
         return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(){
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+        entityManagerFactoryBean.setPackagesToScan(environment.getRequiredProperty("db.entitymanager.packages.to.scan"));
+
+        entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(){
+
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+
+        return transactionManager;
+    }
+
+    private Properties getHibernateProperties() {
+
+        Properties properties = new Properties();
+
+        properties.put("db.hibernate.dialect", environment.getRequiredProperty("db.hibernate.dialect"));
+        properties.put("db.hibernate.show_sql", environment.getRequiredProperty("db.hibernate.show_sql"));
+        properties.put("db.hibernate.hbm2ddl.auto", environment.getRequiredProperty("db.hibernate.hbm2ddl.auto"));
+
+        return properties;
     }
 
 }
